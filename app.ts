6188c765e51dd6b1420f66c204650f0443ba98c1
sandunsameera <sandun@yaalalabs.com>
2023-03-03 13:57:07 +0530

    make it better

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import authRoutes from './routes/auth';
 import userRoutes from './routes/user';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
 // Middleware for parsing JSON data in requests
 app.use(express.json());
